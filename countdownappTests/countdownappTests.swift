4bf85d5e546e7893f755c9f8792494e6b0f072c9
Takahashi Shota <shohh6119@gmial.com>
2026-02-12 01:23:32 +0900

    Initial Commit

---
+++ countdownappTests/countdownappTests.swift
@@ -0,0 +1,17 @@
+//
+//  countdownappTests.swift
+//  countdownappTests
+//
+//  Created by Takahashi Shohta on 2/12/26.
+//
+
+import Testing
+@testable import countdownapp
+
+struct countdownappTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
